d9659174ae4a4cff4bcd49c6142a6abad27bc792
Austin Dizzy <dizzy@wow.com>
2014-10-31 15:27:56 -0400

    Added configuration.

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"crypto/hmac"
 	"crypto/sha1"
+	"encoding/json"
 	"fmt"
 	"github.com/martini-contrib/auth"
 	"io/ioutil"
@@ -10,35 +11,77 @@ import (
 	"net/http"
 	"os"
 	"os/exec"
+	"path/filepath"
 )
 
+type Config struct {
+	HookPath string  `json:"hookPath"`
+	HookPort string  `json:"hookPort"`
+	Sites    []Repos `json:"repos"`
+}
+
+type Repos struct {
+	Repository string `json:"repository"`
+	SecureKey  string `json:"secureKey"`
+	Dir        string `json:"dir"`
+}
+
+type GHPayload struct {
+	Repository struct {
+		FullName string `json:"full_name"`
+	} `json:"repository"`
+}
+
+var config *Config
+
+func init() {
+	dir, _ := filepath.Abs(filepath.Dir(os.Args[0]))
+	c, err := ioutil.ReadFile(dir + "/config.json")
+	if err != nil {
+		log.Println(err)
+	}
+	err = json.Unmarshal(c, &config)
+	if err != nil {
+		log.Println(err)
+	}
+	log.Println("Loaded config:", config)
+}
+
 func main() {
-	log.Println("Starting server...")
-	http.HandleFunc("/github", gitHandle)
-	http.ListenAndServe(":8800", nil)
+	log.Println("Starting server on port", config.HookPort+"...")
+	http.HandleFunc(config.HookPath, gitHandle)
+	http.ListenAndServe(config.HookPort, nil)
 }
 
 func gitHandle(w http.ResponseWriter, r *http.Request) {
 	body, _ := ioutil.ReadAll(r.Body)
 	defer r.Body.Close()
+
+	var ghPayload GHPayload
+	_ = json.Unmarshal(body, &ghPayload)
+
+	log.Println(ghPayload.Repository.FullName)
 	if r.Method == "POST" && r.Header["X-Github-Event"][0] == "push" {
-		mac := hmac.New(sha1.New, []byte("testinghmac"))
-		mac.Reset()
-		mac.Write(body)
-		macSum := fmt.Sprintf("sha1=%x", mac.Sum(nil))
-		log.Println(macSum)
-		log.Println(r.Header["X-Hub-Signature"][0])
-		log.Println(auth.SecureCompare(r.Header["X-Hub-Signature"][0], macSum))
-		log.Println(r)
-		os.Chdir("/var/www/austindizzy.me/")
-		cmd := exec.Command("git", "pull")
-		out, err := cmd.Output()
+		for _, repo := range config.Sites {
+			if repo.Repository == ghPayload.Repository.FullName {
+				mac := hmac.New(sha1.New, []byte(repo.SecureKey))
+				mac.Reset()
+				mac.Write(body)
+				macSum := fmt.Sprintf("sha1=%x", mac.Sum(nil))
+				if auth.SecureCompare(r.Header["X-Hub-Signature"][0], macSum) {
+					os.Chdir(repo.Dir)
+					cmd := exec.Command("git", "pull")
+					out, err := cmd.Output()
 
-		if err != nil {
-			log.Println(err.Error())
-			return
+					if err != nil {
+						log.Println(err.Error())
+					}
+
+					log.Println(string(out))
+				} else {
+					log.Println("UNAUTHORIZED REQUEST:", string(body))
+				}
+			}
 		}
-
-		log.Println(string(out))
 	}
 }
